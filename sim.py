396cc6f133aef2251c240b1d73467662a38aea31
Eric Chandler <echandler@cs.uchicago.edu>
2021-06-28 01:54:11 -0500

    facilitate multiple monte carlo and parameter value trials

--- sim.py
+++ sim.py
@@ -8,23 +8,22 @@ class Sim():
 
     def __init__(self):
         # Parameters
-        self.N_FAMILIES = 100
+        self.N_FAMILIES = 50
         self.N_TIMESTEPS = 100
         self.N_NEIGHBORHOODS = 2
         self.INCOME_GROWTH = 0   # alpha in eq(1)
         self.INCOME_MOBILITY_COEF = 0  # beta in eq(1)
         self.INCOME_NOISE_AUTOREG = 0 
-        self.TAX_RATE = 1
+        self.TAX_RATE = .5
         self.PREF_CONSUMPTION = .5
         self.PREF_INCOME = 1 - self.PREF_CONSUMPTION
         self.rng = default_rng(seed=12345)
 
-    def set(self, params):
-        for key in params.keys():
-            if key not in self:
-                print(f"Error: Invalid parameter {key}")
-                continue
-            self[key] = params[key]
+    def set(self, param, value):
+        if hasattr(self, param):
+            setattr(self, param, value)
+        else:
+            print(f"Error: Invalid parameter {param}")
     
     def __generate_white_noise(self, size, mean=0, var=1):
         """returns `size` random samples"""
@@ -120,7 +119,7 @@ class Sim():
             n_size[n] = (adult_neighborhood == n).sum()
         return n_size
 
-    def run(self):
+    def __run(self):
         income = np.zeros((self.N_TIMESTEPS, self.N_FAMILIES))
         neighborhoods = np.zeros((self.N_TIMESTEPS, self.N_FAMILIES))
         neighborhood_size = np.zeros((self.N_TIMESTEPS, self.N_NEIGHBORHOODS))
@@ -142,21 +141,117 @@ class Sim():
         
         return SimResult(income, neighborhoods, neighborhood_size, human_capital)
 
+    def run(self, ntrials=1, keep_trials=False):
+        result = SimResultAgg(keep_trials)
+        for trial in range(ntrials):
+            result.add(self.__run())
+        return result
+
+    def run_sweep(self, param, values, ntrials=1):
+        result = SimResultSweep()
+        for value in values:
+            self.set(param, value)
+            result.add(self.run(ntrials), value)
+        return result
+
+
+class SimResultData():
+        def __init__(self, result, name):
+            self.data = result
+            self.name = name
+
+        def plot(self):
+            sns.relplot(data=self.data, kind='line', legend=False).set(title=self.name)
+    
+
 class SimResult():
     def __init__(self, income, neighborhoods, neighborhood_size, human_capital):
-        self.income = income
-        self.neighborhood = neighborhoods
-        self.neighborhood_size = neighborhood_size
-        self.human_capital = human_capital
+        self.income = SimResultData(income, "Income")
+        self.neighborhood = SimResultData(neighborhoods, "Neighborhoods")
+        self.neighborhood_size = SimResultData(neighborhood_size, "Neighborhood Size")
+        self.human_capital = SimResultData(human_capital, "Capital")
+
+
+class SimResultAggData():
+    def __init__(self, name, keep_trials=False):
+        self.data = pd.DataFrame()
+        self.name = name
+        self.ntrials = 0
+        self.trial_data = []
+        self.keep_trials = keep_trials
+
+    def add(self, result):
+        self.data = pd.concat([self.data, 
+                        pd.Series(result.data.mean(axis=1), name=f"mean_{self.ntrials}"), 
+                        pd.Series(result.data.std(axis=1), name=f"sd_{self.ntrials}")], 
+                        axis=1)
+        self.ntrials += 1
+        if self.keep_trials:
+            self.trial_data.append(result)
+
+    def plot(self):
+        if self.ntrials == 0:
+            print("Error: Nothing to plot")
+            return
+        self.data.index.rename('Time', inplace=True)
+        pdata = self.data.reset_index().melt(id_vars='Time')
+        pdata = pd.concat([pdata.Time, pdata.variable.str.split("_", expand=True, n=1), pdata.value], axis=1)
+        assert(len(pdata.columns) == 4)
+        pdata.columns = ['Time','Agg','Trial','Value']
+        rplt = sns.relplot(data=pdata, x='Time', y='Value', col='Agg', hue='Trial',
+                    kind='line') \
+                .set_titles(col_template="{col_name}")
+        rplt.fig.subplots_adjust(top=0.9)
+        rplt.fig.suptitle(self.name)
+
+
+class SimResultAgg():
+    def __init__(self, keep_trials=False):
+        self.ntrials = 0
+        self.income = SimResultAggData("Income", keep_trials)
+        self.neighborhood_size = SimResultAggData("Neighborhood Size", keep_trials)
+        self.human_capital = SimResultAggData("Capital", keep_trials)
     
-    def plot_income(self):
-        sns.relplot(data=self.income, kind='line', legend=False).set(title='Income')
+    def add(self, result):
+        self.income.add(result.income)
+        self.neighborhood_size.add(result.neighborhood_size)
+        self.human_capital.add(result.human_capital)
+
+
+class SimResultSweepData():
+    def __init__(self, name):
+        self.data = pd.DataFrame()
+        self.name = name
+
+    def add(self, result, param_val):
+        param_idx = pd.Index([param_val]).repeat(result.data.shape[0])
+        tuples = list(zip(param_idx, np.arange(result.data.shape[0])))
+        new_idx = pd.MultiIndex.from_tuples(tuples, names=["Param", "Time"])
+        result_data = pd.concat([pd.Series(result.data.mean(axis=1), name=f"mean"), 
+                                pd.Series(result.data.std(axis=1), name=f"sd")], 
+                                axis=1)  
+        result_data.set_index(new_idx, inplace=True)
+        self.data = pd.concat([self.data, result_data], axis=0)
     
-    def plot_neighborhoods(self):
-        sns.relplot(data=self.neighborhoods, kind='line' ,legend=False).set(title='Neighborhood')
+    def plot(self):
+        if self.data.empty:
+            print("Error: Nothing to plot")
+            return
+        pdata = self.data.reset_index().melt(id_vars=['Param','Time'],var_name='Agg')
+        rplt = sns.relplot(data=pdata, x='Time', y='value', col='Agg', hue='Param',
+                    kind='line') \
+                .set_titles(col_template="{col_name}")
+        rplt.fig.subplots_adjust(top=0.9)
+        rplt.fig.suptitle(self.name)
+
     
-    def plot_neighborhood_size(self):
-        sns.relplot(data=self.neighborhood_size, kind='line' ,legend=False).set(title='Neighborhood Size')
+class SimResultSweep():
+    def __init__(self):
+        self.income = SimResultSweepData("Income")
+        self.neighborhood_size = SimResultSweepData("Neighborhood Size")
+        self.human_capital = SimResultSweepData("Capital")
     
-    def plot_human_capital(self):
-        sns.relplot(data=self.human_capital, kind='line', legend=False).set(title='Capital')
+    def add(self, result, param_val):
+        self.income.add(result.income, param_val)
+        self.neighborhood_size.add(result.neighborhood_size, param_val)
+        self.human_capital.add(result.human_capital, param_val)
