4a6ac5361d1d73bc62ac553760b3813f958d2553
Eric Chandler <echandler@uchicago.edu>
2021-08-14 12:31:06 -0500

    bound human capital to prevent log(-x)

--- human_capital.py
+++ human_capital.py
@@ -14,7 +14,8 @@ class HumanCapital():
         hc = np.full(self.config.N_FAMILIES, 1)
         return hc
 
-    def __compute_edu_efficiency(self, school_size):
+
+    def _compute_edu_efficiency(self, school_size):
         upper = .9 # lambda_2 in eq (8)
         lower = .1 # lambda_1 in eq (8)
         scale = 10 / self.config.N_FAMILIES
@@ -28,7 +29,7 @@ class HumanCapital():
         ed = np.zeros(self.config.N_FAMILIES)
         for n in range(self.config.N_NEIGHBORHOODS):
             n_size = (parent_neighborhood == n).sum()
-            econ_of_scale = self.__compute_edu_efficiency(n_size)
+            econ_of_scale = self._compute_edu_efficiency(n_size)
             ed[parent_neighborhood == n] = taxbase[n] / econ_of_scale
         return ed
 
@@ -38,13 +39,13 @@ class HumanCapital():
         # For now arbitrarily use income * avg(neighborhood_income)
         skill = np.zeros(self.config.N_FAMILIES)
         for n in range(self.config.N_NEIGHBORHOODS):
-            # bound below by 0 so we dont get negative skill
-            # TODO: actually fix/prevent negative skill due to negative income
-            par_income = np.maximum(0, parent_income[parent_neighborhood == n])
-            # eq (10) excludes parent income from avg
+            # bound below by 1 so we dont get negative skill
+            # TODO: actually fix/prevent negative skill due to low income
+            par_income = np.maximum(1, parent_income[parent_neighborhood == n])
+            # exclude parent income from avg <- eq (10) 
             avg_income = (par_income.sum() - par_income) / (par_income.size - 1)
-            skill[parent_neighborhood == n] = np.power(par_income, self.config.SKILL_FROM_PARENT_INCOME) * \
-                                              np.power(avg_income, self.config.SKILL_FROM_NEIGHBOR_INCOME)
+            skill[parent_neighborhood == n] = np.log(par_income) * self.config.SKILL_FROM_PARENT_INCOME + \
+                                              np.log(avg_income) * self.config.SKILL_FROM_NEIGHBOR_INCOME
         return skill
 
 
